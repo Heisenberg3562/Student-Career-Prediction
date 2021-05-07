05189413fb8159833c1d0ea3efb429e816331137
Omkar Babrekar <babrekaromkar@gmail.com>
2021-05-08 00:43:23 +0530

    Update settings.py

--- prediction/settings.py
+++ prediction/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR,'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
