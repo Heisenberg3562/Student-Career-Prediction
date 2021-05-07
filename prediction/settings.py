298acd254ac343d082d0214831343f2b2a5f2f0d
Omkar Babrekar <babrekaromkar@gmail.com>
2021-05-08 01:39:25 +0530

    Update settings.py

--- prediction/settings.py
+++ prediction/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'mrh_fxn936rs#chk0^a456&0gw#h)ej67-uotk3$yy(90y!-l9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://career4hub.herokuapp.com','career4hub.herokuapp.com']
+ALLOWED_HOSTS = ['https://career4hub.herokuapp.com','career4hub.herokuapp.com','.herokuapp.com']
 
 
 # Application definition
