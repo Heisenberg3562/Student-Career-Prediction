1e997ed316282761ad03dabbbc1a8de65c659102
Omkar Babrekar <babrekaromkar@gmail.com>
2021-05-08 01:02:56 +0530

    Update settings.py

--- prediction/settings.py
+++ prediction/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mrh_fxn936rs#chk0^a456&0gw#h)ej67-uotk3$yy(90y!-l9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://career4hub.herokuapp.com/']
+ALLOWED_HOSTS = ['https://career4hub.herokuapp.com','career4hub.herokuapp.com']
 
 
 # Application definition
