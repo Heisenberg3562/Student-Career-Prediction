55b8fc66b9ac4602f8e0a3ce66760842830f2c90
Omkar Babrekar <babrekaromkar@gmail.com>
2021-05-08 19:47:24 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -46,4 +46,4 @@ def predict(request):
         results = model.predict(df)
         prediction = labelencoder.inverse_transform(results)
         criteria['prediction'] = prediction[0]
-    return JsonResponse(criteria, safe=False)
+    return Response(criteria)
