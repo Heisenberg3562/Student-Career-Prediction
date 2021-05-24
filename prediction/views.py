43d9a8da3a1c9dff5e6756320c8ee80c434a29ab
Heisenberg3562 <babrekaromkar@gmail.com>
2021-05-25 00:19:54 +0530

    Update views.py

--- prediction/views.py
+++ prediction/views.py
@@ -6,12 +6,11 @@ import pandas as pd
 
 def index(request):
     form = request.POST or None
-    criteria = {"name":"Omkar"}
+    criteria = {}
     if form:
         print(request.POST)
         input = dict()
         li = []
-        criteria = {"name":"Omkar"}
         columns=['Acedamic percentage in Operating Systems', 'percentage in Algorithms',
                        'Percentage in Programming Concepts',
                        'Percentage in Software Engineering', 'Percentage in Computer Networks',
@@ -23,19 +22,30 @@ def index(request):
                        'self-learning capability?', 'Extra-courses did', 'olympiads',
                        'reading and writing skills', 'Management or Technical', 'hard/smart worker',
                        'worked in teams ever?']
-        for i,j in zip(range(len(request.POST)-1),columns):
+        for i in range(len(request.POST)-1):
             li.append(int(request.POST['input'+str(i)]))
         normalized_data = Normalizer().fit_transform([li])
         print(normalized_data)
         for i,j in zip(range(len(request.POST)-1),columns):
             input[j] = normalized_data[0][i]
         print(input)
-        df = pd.DataFrame(input,index=[0])
+        df = pd.DataFrame(input,index=[0],columns=['Acedamic percentage in Operating Systems', 'percentage in Algorithms',
+       'Percentage in Programming Concepts',
+       'Percentage in Software Engineering', 'Percentage in Computer Networks',
+       'Percentage in Electronics Subjects',
+       'Percentage in Computer Architecture', 'Percentage in Mathematics',
+       'Percentage in Communication skills', 'Hours working per day',
+       'Logical quotient rating', 'hackathons', 'coding skills rating',
+       'public speaking points', 'can work long time before system?',
+       'self-learning capability?', 'Extra-courses did', 'olympiads',
+       'reading and writing skills', 'Management or Technical', 'hard/smart worker',
+       'worked in teams ever?'])
         print(df)
         model = joblib.load('static/svm.pkl')
         labelencoder = joblib.load('static/label.pkl')
         results = model.predict(df)
         print(results)
+        results = pd.DataFrame(results, columns=["Suggested Job Role"])
         prediction = labelencoder.inverse_transform(results)
         print(prediction)
         criteria['prediction'] = prediction[0]
